47733010193de4545ddb9451eb1464e7a4ae6483
Pooja Goyal <poojagl85@gmail.com>
2021-11-27 09:12:17 +0530

    login issue solved

--- src/utils/user.action.js
+++ src/utils/user.action.js
@@ -4,7 +4,9 @@ export const isUserLoggedIn = () => {
 	return async (dispatch) => {
 
 		const user = window.localStorage.getItem("user");
-		if (user) {
+
+		if (user && document.cookie !== '') {
+			console.log("In hi")
 			const parseUser = JSON.parse(user);
 			dispatch({
 				type: userConstants.SIGNIN_SUCCESS,
@@ -13,6 +15,9 @@ export const isUserLoggedIn = () => {
 				},
 			});
 		} else {
+			if (user) {
+				window.localStorage.removeItem("user");
+			}
 			dispatch({
 				type: userConstants.SIGNIN_FAILURE,
 				payload: {
